0bdfcfaffe527f15c448045b65c460614534a454
Caio <caio.lindstrom@hotmail.com>
2022-12-27 16:11:53 -0300

    aprendendo sobre DOM e testando novas funções no código. Event Listener retornando corretamente as funcoes.

--- code.js
+++ code.js
@@ -1,81 +1,52 @@
-function getComputerChoice(){
-    options = ["rock" , "paper", "scissors"];
-    var result = options[Math.floor(Math.random() * 3)];
-    console.log(result);
-    return result;
-      
+
+
+
+function getRock(){
+    var choice = "rock";
+    console.log(choice);
+    return choice;
+    
 }
 
-function getPlayerChoice(){
-    var choice = prompt("Choose between Rock, Paper and Scissors").toLowerCase();
+function getPaper(){
+    var choice = "paper";
     console.log(choice);
     return choice;
 }
 
-function checkWinner (getPlayerChoice, getComputerChoice){
-    if(getPlayerChoice === getComputerChoice) {
-        return "Tie";
-    }
-    else if (
-        (getPlayerChoice === "rock" && getComputerChoice === "scissors") ||
-        (getPlayerChoice === "paper" && getComputerChoice === "rock") ||
-        (getPlayerChoice === "scissors" && getComputerChoice === "paper")
-    ) {
-        return "Player";
-    }
-    else {
-        return "Computer";
-    }
-    
-             
+function getScissors(){
+    var choice = "scissors";
+    console.log(choice);
+    return choice;
 }
-    function playRound (getPlayerChoice, getComputerChoice) {
-        let roundResult = checkWinner(getPlayerChoice, getComputerChoice);
 
-        if (roundResult === "Player") {
-            return "You won this round!"
-             
-        } else if (roundResult === "Computer") {
-            return "You lost this round!"
-            
-        
-    } else {
-        return "It was a Tie!";
-    }
- }
-        
+document.getElementById("rock").addEventListener("click", getRock);
+document.getElementById("paper").addEventListener("click", getPaper);
+document.getElementById("scissors").addEventListener("click", getScissors);
 
-function game() {
-    console.log("Game Start!");
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("-------")
-        if (checkWinner(playerSelection, computerSelection) == "Player"){
-            playerScore++;
-        }   
-     else if (checkWinner(playerSelection, computerSelection) == "Computer"){
-            computerScore++;
-    
+
+function getComputerChoice(){
+    choices = ["rock", "paper", "scissors"];
+    var computer = choices[Math.floor(Math.random() * 3)];
+    console.log(computer);
+    return computer;
+}
+
+
+
+function checkWinner() {
+    if (getPlayerChoice === getComputerChoice) {
+        return "Its a Tie";
+    } else if (
+        (getPlayerChoice === "rock" && getComputerChoice === "scissors") ||
+            (getPlayerChoice === "paper" && getComputerChoice === "rock") ||
+            (getPlayerChoice === "scissors" && getComputerChoice === "paper")
+        ){
+            return "Player";
         }
+        else {
+            return "Computer";
+        }
+    
     }
-    console.log("Game Over")
-    if (playerScore > computerScore){
-        console.log ("Player Wins");
-    }
-    else if (playerScore < computerScore) {
-        console.log ("Computer WIns");
-    }
-    else{
-        console.log("Tie!");
-    }
- }
-
-
-   game();
-
 
