2ae1c7b876afaa691aaa0122e4c0c7284acc2b14
Caio <caio.lindstrom@hotmail.com>
2022-12-29 13:32:57 -0300

    necessario estudar funcoes arrow. Foram feitas alteracoes com base em pesquisa para entender melhor como encurtar o codigo e manipular o DOM para aderir funcoes aos botoes e retornar o resultado de um Round

--- code.js
+++ code.js
@@ -1,74 +1,43 @@
 
+const options = document.querySelectorAll(".options");
 
+options.forEach((option) => {
+    option.addEventListener("click", function () {
+        const getPlayerChoice = this.textContent;
+        console.log(getPlayerChoice);
+        var choices = ["rock", "paper", "scissors"];
+        const getComputerChoice = choices[Math.floor(Math.random() * 3)];
+        console.log(getComputerChoice);
+        checkWinner(getPlayerChoice, getComputerChoice)
 
-function getRock(){
-    var choice = "rock";
-    console.log(choice);
-    return choice;
-    
-}
-
-function getPaper(){
-    var choice = "paper";
-    console.log(choice);
-    return choice;
-}
-
-function getScissors(){
-    var choice = "scissors";
-    console.log(choice);
-    return choice;
-}
-
-
-
-
-function getComputerChoice(){
-    choices = ["rock", "paper", "scissors"];
-    var computer = choices[Math.floor(Math.random() * 3)];
-    console.log(computer);
-    return computer;
-}
+    });
+});
 
 
 
 function checkWinner(getPlayerChoice, getComputerChoice) {
     if (getPlayerChoice === getComputerChoice) {
+        console.log("Tie!");
         return "Its a Tie";
     } else if (
         (getPlayerChoice === "rock" && getComputerChoice === "scissors") ||
             (getPlayerChoice === "paper" && getComputerChoice === "rock") ||
             (getPlayerChoice === "scissors" && getComputerChoice === "paper")
-        ){
+        ){  
+            console.log("Venceu!")
             return "Player";
+            
         }
         else {
+            console.log("Perdeu!")
             return "Computer";
+            
         }
     
     }
 
-    function playRound(getPlayerChoice, getComputerChoice) {
-        var result = checkWinner(getPlayerChoice, getComputerChoice);
-        if (result === "Player"){
-            return "You won this round!"
-        }
-        else if (result === "Computer") {
-            return "You lost this round!"
-        }
-        else {
-            return "It was a Tie!"
-        }
-    }
+   
 
-    document.getElementById("rock").addEventListener("click", getRock);
-    document.getElementById("paper").addEventListener("click", getPaper);
-    document.getElementById("scissors").addEventListener("click", getScissors);
-    document.getElementById("rock").addEventListener("click", getComputerChoice);
-    document.getElementById("paper").addEventListener("click", getComputerChoice);
-    document.getElementById("scissors").addEventListener("click", getComputerChoice);
-    document.getElementById("rock").addEventListener("click", playRound);
-    document.getElementById("paper").addEventListener("click", playRound);
-    document.getElementById("scissors").addEventListener("click", playRound);
+   
 
-    console.log(playRound(getPlayerChoice, getComputerChoice))
+   
