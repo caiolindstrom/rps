d5da412379447d9fb6017502b25060285ce4c1c3
Caio <caio.lindstrom@hotmail.com>
2022-12-30 15:48:11 -0300

    mudancas feitas no codigo para tentar implementar tudo em apenas uma funcao. sem sucesso. tentarei criar mais duas funcoes para gerar rounds e gerar o loop com o while.

--- code.js
+++ code.js
@@ -1,21 +1,31 @@
 
+
+
 const options = document.querySelectorAll(".options");
 
 options.forEach((option) => {
     option.addEventListener("click", function () {
+        
         const getPlayerChoice = this.textContent;
         console.log(getPlayerChoice);
+
         var choices = ["rock", "paper", "scissors"];
         const getComputerChoice = choices[Math.floor(Math.random() * 3)];
         console.log(getComputerChoice);
-        checkWinner(getPlayerChoice, getComputerChoice)
+        checkWinner(getPlayerChoice, getComputerChoice);
 
     });
 });
 
 
 
+
 function checkWinner(getPlayerChoice, getComputerChoice) {
+    
+    var playerScore = 0;
+    var computerScore = 0;
+    
+
     if (getPlayerChoice === getComputerChoice) {
         console.log("Tie!");
         return "Its a Tie";
@@ -24,20 +34,30 @@ function checkWinner(getPlayerChoice, getComputerChoice) {
             (getPlayerChoice === "paper" && getComputerChoice === "rock") ||
             (getPlayerChoice === "scissors" && getComputerChoice === "paper")
         ){  
-            console.log("Venceu!")
+            console.log("You Win!")
+            playerScore++;
+            document.getElementById("pScore").innerHTML = playerScore;
             return "Player";
             
         }
         else {
-            console.log("Perdeu!")
+            console.log("You lose!")
+            computerScore++;
+            document.getElementById("cScore").innerHTML = computerScore;
             return "Computer";
             
         }
-    
+
+        
     }
 
-   
+function game() {
 
-   
+        while (resultOne || resultTwo < 5) {
+            
 
-   
+            var resultOne = playerScore;
+            var resultTwo = computerScore;
+            checkWinner(getPlayerChoice, getComputerChoice);
+        }
+}
